d97edadeb2708d60a84f4e88619720a4b013bc42
Jesús Mendoza <133887362+JessEleven@users.noreply.github.com>
2025-04-20 23:17:36 -0500

    added GET and DELETE methods to schema-metadata API

--- src/app/api/schema-metadata/route.js
+++ src/app/api/schema-metadata/route.js
@@ -1,8 +1,53 @@
 import { db } from '@/database/drizzle'
 import { schemaMetadata } from '@/database/schema'
-import { and, eq } from 'drizzle-orm'
+import { auth } from '@/libs/auth'
+import { and, desc, eq } from 'drizzle-orm'
+import { headers } from 'next/headers'
 import { NextResponse } from 'next/server'
 import { v4 as uuidv4 } from 'uuid'
+
+export async function GET () {
+  try {
+    const { user } = await auth.api.getSession({
+      headers: await headers()
+    })
+
+    if (!user.id) {
+      return NextResponse.json({
+        success: false,
+        status_code: 401,
+        message: 'User is not authenticated'
+      }, { status: 401 })
+    }
+    const allProjects = await db.select()
+      .from(schemaMetadata)
+      .where(eq(schemaMetadata.userId, user.id))
+      .orderBy(desc(schemaMetadata.createdAt))
+
+    if (allProjects.length === 0) {
+      return NextResponse.json({
+        success: false,
+        status_code: 200,
+        message: 'The project list is empty',
+        data: []
+      })
+    }
+    return NextResponse.json({
+      success: true,
+      status_code: 200,
+      message: `The list of projects is ${allProjects.length}`,
+      owner: user.name,
+      email: user.email,
+      data: allProjects
+    })
+  } catch (error) {
+    return NextResponse.json({
+      success: false,
+      status_code: 500,
+      message: error.message
+    }, { status: 500 })
+  }
+}
 
 export async function POST (req) {
   try {
@@ -39,6 +84,50 @@ export async function POST (req) {
       message: 'Project created successfully',
       data: result
     }, { status: 201 })
+  } catch (error) {
+    return NextResponse.json({
+      success: false,
+      status_code: 500,
+      message: error.message
+    }, { status: 500 })
+  }
+}
+
+export async function DELETE (req) {
+  try {
+    const { user } = await auth.api.getSession({
+      headers: await headers()
+    })
+
+    if (!user.id) {
+      return NextResponse.json({
+        success: false,
+        status_code: 401,
+        message: 'User is not authenticated'
+      }, { status: 401 })
+    }
+    const { id } = await req.json()
+
+    if (!id) {
+      return NextResponse.json({
+        success: false,
+        status_code: 400,
+        message: 'ID is missing to delete the project'
+      }, { status: 400 })
+    }
+    const deleteProject = await db.delete(schemaMetadata)
+      .where(and(
+        eq(schemaMetadata.id, id),
+        eq(schemaMetadata.userId, user.id)
+      ))
+      .returning()
+
+    return NextResponse.json({
+      success: true,
+      status_code: 200,
+      message: 'Project successfully deleted',
+      data: deleteProject
+    })
   } catch (error) {
     return NextResponse.json({
       success: false,
--- src/utils/schema-matadata.js
+++ src/utils/schema-matadata.js
@@ -1,5 +1,23 @@
 import { authClient } from '@/libs/auth-client'
 
+export const getSchemaMetadata = async () => {
+  try {
+    const response = await fetch('/api/schema-metadata', {
+      method: 'GET',
+      headers: { 'Content-Type': 'application/json' }
+    })
+    const result = await response.json()
+
+    if (!response.ok || !result.success) {
+      throw new Error(result.error || 'Failed to fetch user projects')
+    }
+    return result.data
+  } catch (error) {
+    console.error('Unexpected error:', error)
+    return error
+  }
+}
+
 export const createSchemaMetadata = async (formData) => {
   try {
     const { data } = await authClient.getSession()
@@ -18,6 +36,28 @@ export const createSchemaMetadata = async (formData) => {
     })
     const result = await response.json()
     return result
+  } catch (error) {
+    console.error('Unexpected error:', error)
+    return error
+  }
+}
+
+export const deleteSchemaMetadata = async ({ id, onSuccess }) => {
+  try {
+    const response = await fetch('/api/schema-metadata', {
+      method: 'DELETE',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ id })
+    })
+    const result = response.json()
+
+    if (!response.ok || !result.success) {
+      throw new Error(result.error || 'Failed to delete the project')
+    }
+
+    if (onSuccess) onSuccess()
+
+    return result.data
   } catch (error) {
     console.error('Unexpected error:', error)
     return error
